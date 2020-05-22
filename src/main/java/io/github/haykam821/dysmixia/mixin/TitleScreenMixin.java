a446cd70b4cd235df754dd996eb655456f0faf7e
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-05-21 23:12:02 -0400

    Implement basic functionality

---
+++ src/main/java/io/github/haykam821/dysmixia/mixin/TitleScreenMixin.java
@@ -0,0 +1,20 @@
+package io.github.haykam821.dysmixia.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+	@Shadow
+	private boolean isMinceraft;
+
+	@Inject(method = "<init>*", at = @At("TAIL"))
+	public void forceMinceraft(CallbackInfo ci) {
+		this.isMinceraft = true;
+	}
+}
